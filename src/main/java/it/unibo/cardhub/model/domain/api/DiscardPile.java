992c94bb05d3b818b6a4c0c7771cbb0ccc9383b3
Kaan Portolani <kaan.portolani@studio.unibo.it>
2026-07-09 00:14:00 +0200

    Refine discard pile API

--- src/main/java/it/unibo/cardhub/model/domain/api/DiscardPile.java
+++ src/main/java/it/unibo/cardhub/model/domain/api/DiscardPile.java
@@ -6,6 +6,13 @@ package it.unibo.cardhub.model.domain.api;
 public interface DiscardPile extends CardCollection {
 
     /**
+     * Returns the most recently discarded card without removing it.
+     *
+     * @return the most recently discarded card
+     */
+    Card peekCard();
+
+    /**
      * Reshuffles the cards into the deck.
      *
      * @param deck of the match
