68def0979e0f8709ce73f75e1a919cecc41fb5ac
Tobias-Basilisco <tobias.basilisco@studio.unibo.it>
2026-07-11 11:44:59 +0200

    CHTextArea

--- src/main/java/it/unibo/cardhub/view/components/CHTextArea.java
+++ src/main/java/it/unibo/cardhub/view/components/CHTextArea.java
@@ -1,9 +1,17 @@
 package it.unibo.cardhub.view.components;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JTextArea;
+import javax.swing.border.Border;
 
 public class CHTextArea extends JTextArea {
     public CHTextArea(){
-
+        this.setBackground(new Color(CHColor.SECONDARY.getCode()));
+        this.setForeground(new Color(CHColor.TERTIARY.getCode()));
+        this.setPreferredSize(new Dimension(100, 20));
+        this.setBorder(BorderFactory.createLineBorder(new Color(CHColor.TERTIARY.getCode())));
     }
 }
