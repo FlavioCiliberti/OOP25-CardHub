a5609513b86590ca90032f2f8a32923eead5db4e
Flavio <flavio.ciliberti@studio.unibo.it>
2026-07-15 09:51:14 +0200

    returned CHFrame to its original state after being done with tests

--- src/main/java/it/unibo/cardhub/view/components/CHFrame.java
+++ src/main/java/it/unibo/cardhub/view/components/CHFrame.java
@@ -22,15 +22,15 @@ public class CHFrame extends JFrame {
         JPanel screen1 = new JPanel();
         screen1.setBackground(Color.green);
         screen1.setBackground(new Color(CHColor.TERTIARY.getCode()));
-        JPanel screen2 = new CreateMatchImpl();
+        JPanel screen2 = new HomeViewImpl();
 
         container.add(screen1, "1");
         container.add(screen2, "2");
 
         this.add(container);
         layout.show(container, "2");
-        //this.setSize(new Dimension(640, 400));
-        this.setSize(new Dimension(470, 710));
+        this.setSize(new Dimension(640, 400));
+        //this.setSize(new Dimension(470, 710));
 
         this.setVisible(true);
     }
