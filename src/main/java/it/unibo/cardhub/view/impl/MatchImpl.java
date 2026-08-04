c28264b27b060653ffb10bb4bef9ca30f51b6cc8
Flavio Ciliberti <flavio.ciliberti@studio.unibo.it>
2026-08-04 23:12:58 +0200

    created MatchImpl

---
+++ src/main/java/it/unibo/cardhub/view/impl/MatchImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.cardhub.view.impl;
+
+import java.awt.BorderLayout;
+
+import it.unibo.cardhub.view.components.ScreenView;
+
+public final class MatchImpl extends ScreenView {
+    
+    public MatchImpl() {
+
+    }
+
+    private void ManageContentPane() {
+        this.setLayout(new BorderLayout());
+    }
+}
