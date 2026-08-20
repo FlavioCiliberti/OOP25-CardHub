94711d62065898a47be6b7213d663e8e7fb7ed3b
Tobias-Basilisco <tobias.basilisco@studio.unibo.it>
2026-08-20 19:58:11 +0200

    final CHFrame and App

--- src/main/java/it/unibo/cardhub/App.java
+++ src/main/java/it/unibo/cardhub/App.java
@@ -1,14 +1,10 @@
 package it.unibo.cardhub;
 
 import it.unibo.cardhub.controller.ScreenId;
-import it.unibo.cardhub.controller.api.MatchController;
+import it.unibo.cardhub.controller.api.HomeController;
 import it.unibo.cardhub.controller.api.Navigator;
 import it.unibo.cardhub.controller.impl.CardLayoutNavigator;
-import it.unibo.cardhub.controller.impl.MatchControllerImpl;
-import it.unibo.cardhub.io.api.DeckFactory;
-import it.unibo.cardhub.io.impl.DeckFactoryImpl;
-import it.unibo.cardhub.model.domain.api.Deck;
-import it.unibo.cardhub.model.logic.impl.MatchFactory;
+import it.unibo.cardhub.controller.impl.HomeControllerImpl;
 import it.unibo.cardhub.view.components.CHFrame;
 
 /**
@@ -31,15 +27,10 @@ public final class App {
         final CHFrame frame = new CHFrame();
         frame.add(navigator.getRootView());
 
-        final DeckFactory deckFactory = new DeckFactoryImpl();
-
-        final Deck deck1 = deckFactory.createPokemonDeck();
-        final Deck deck2 = deckFactory.createPokemonDeck();
-
-        final MatchController match = new MatchControllerImpl(
-                                    MatchFactory.createFreeMatch("Ash", 
-                                    "Misty", deck1, deck2), navigator);
-        navigator.show(ScreenId.MATCH, match.getView());
+        final HomeController home = new HomeControllerImpl(
+                                    navigator,
+                                    frame::dispose);
+        navigator.show(ScreenId.HOME, home.getView());
 
         frame.setVisible(true);
     }
