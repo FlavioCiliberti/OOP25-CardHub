86c1143b42ad6d3350d33afd8ea72602139cd5e8
Flavio Ciliberti <flavio.ciliberti@studio.unibo.it>
2026-08-24 19:14:27 +0200

    added a builder to MatchFactory

--- src/main/java/it/unibo/cardhub/model/logic/impl/MatchFactory.java
+++ src/main/java/it/unibo/cardhub/model/logic/impl/MatchFactory.java
@@ -1,5 +1,7 @@
 package it.unibo.cardhub.model.logic.impl;
 
+import java.util.Objects;
+
 import it.unibo.cardhub.io.api.DeckFactory;
 import it.unibo.cardhub.io.impl.DeckFactoryImpl;
 import it.unibo.cardhub.model.domain.api.Deck;
@@ -7,15 +9,12 @@ import it.unibo.cardhub.model.domain.api.Player;
 import it.unibo.cardhub.model.domain.impl.PlayerImpl;
 import it.unibo.cardhub.model.logic.api.CardAction;
 import it.unibo.cardhub.model.logic.api.Match;
+import it.unibo.cardhub.model.logic.api.MatchLogic;
 
 /**
  * a factory for Match.
  */
 public final class MatchFactory {
-    private static final int DEFAULT_MAX_HAND_SIZE = 5;
-    private static final int DEFAULT_STARTING_HAND_SIZE = 5;
-    private static final int DEFAULT_PLAYFIELD_SIZE = 3;
-
     private static final int ECARD_MAX_HAND_SIZE = 7;
     private static final int ECARD_STARTING_HAND_SIZE = 7;
     private static final int ECARD_PLAYFIELD_SIZE = 1;
@@ -36,14 +35,9 @@ public final class MatchFactory {
     public static Match createFreeMatch(final String firstPlayerName, final String secondPlayerName,
                                         final Deck firstPlayerDeck, final Deck secondPlayerDeck) {
 
-        final Player player1 = new PlayerImpl(firstPlayerName, DEFAULT_MAX_HAND_SIZE,
-                                                DEFAULT_STARTING_HAND_SIZE, firstPlayerDeck);
-        final Player player2 = new PlayerImpl(secondPlayerName, DEFAULT_MAX_HAND_SIZE,
-                                                DEFAULT_STARTING_HAND_SIZE, secondPlayerDeck);
-
-        return new MatchImpl(player1, player2, 
-                                DEFAULT_PLAYFIELD_SIZE, true, 
-                                new MatchLogicImpl(CardAction.TO_PILE, CardAction.TO_PILE));
+        return new MatchBuilder(firstPlayerName, secondPlayerName,
+                                firstPlayerDeck, secondPlayerDeck,
+                                new MatchLogicImpl(CardAction.TO_PILE, CardAction.TO_PILE)).build();
     }
 
     /**
@@ -66,16 +60,11 @@ public final class MatchFactory {
                                             final int maxHandSize, final int startingHandSize,
                                             final int playerFieldSize, final boolean autoDraw,
                                             final CardAction winnerAction, final CardAction loserAction) {
-
-        final Player player1 = new PlayerImpl(firstPlayerName, maxHandSize,
-                                                startingHandSize, firstPlayerDeck);
-        final Player player2 = new PlayerImpl(secondPlayerName, maxHandSize,
-                                                startingHandSize, secondPlayerDeck);
-
-        return new MatchImpl(player1, player2,
-                                playerFieldSize, autoDraw,
-                                new MatchLogicImpl(winnerAction, loserAction));
-
+        return new MatchBuilder(firstPlayerName, secondPlayerName,
+                                firstPlayerDeck, secondPlayerDeck,
+                                new MatchLogicImpl(winnerAction, loserAction))
+                                .maxHandSize(maxHandSize).startingHandSize(startingHandSize)
+                                .playfieldSize(playerFieldSize).autoDraw(autoDraw).build();
     }
 
     /**
@@ -88,13 +77,73 @@ public final class MatchFactory {
     public static Match createECardMatch(final String firstPlayerName, final String secondPlayerName) {
         final DeckFactory deckFactory = new DeckFactoryImpl();
 
-        final Player player1 = new PlayerImpl(firstPlayerName, ECARD_MAX_HAND_SIZE,
-                                                ECARD_STARTING_HAND_SIZE, deckFactory.createECardDeck());
-        final Player player2 = new PlayerImpl(secondPlayerName, ECARD_MAX_HAND_SIZE,
-                                                ECARD_STARTING_HAND_SIZE, deckFactory.createECardDeck());
-
-        return new MatchImpl(player1, player2, 
-                            ECARD_PLAYFIELD_SIZE, false, new ECardLogic());
+        return new MatchBuilder(firstPlayerName, secondPlayerName,
+                                deckFactory.createECardDeck(), deckFactory.createECardDeck(), new ECardLogic())
+                                .maxHandSize(ECARD_MAX_HAND_SIZE).startingHandSize(ECARD_STARTING_HAND_SIZE)
+                                .playfieldSize(ECARD_PLAYFIELD_SIZE).autoDraw(false).build();
     }
+
+    /**
+     * A builder for Match.
+     */
+    private static final class MatchBuilder {
+    private static final int DEFAULT_MAX_HAND_SIZE = 5;
+    private static final int DEFAULT_STARTING_HAND_SIZE = 5;
+    private static final int DEFAULT_PLAYFIELD_SIZE = 3;
+
+    private final String firstPlayerName;
+    private final String secondPlayerName;
+
+    private final Deck firstPlayerDeck;
+    private final Deck secondPlayerDeck;
+
+    private final MatchLogic logic;
+
+    private int maxHandSize = DEFAULT_MAX_HAND_SIZE;
+    private int startingHandSize = DEFAULT_STARTING_HAND_SIZE;
+    private int playfieldSize = DEFAULT_PLAYFIELD_SIZE;
+    private boolean autoDraw = true;
+
+    private MatchBuilder(final String firstPlayerName, final String secondPlayerName,
+                        final Deck firstPlayerDeck, final Deck secondPlayerDeck,
+                        final MatchLogic logic) {
+        this.firstPlayerName = Objects.requireNonNull(firstPlayerName);
+        this.secondPlayerName = Objects.requireNonNull(secondPlayerName);
+
+        this.firstPlayerDeck = Objects.requireNonNull(firstPlayerDeck);
+        this.secondPlayerDeck = Objects.requireNonNull(secondPlayerDeck);
+
+        this.logic = Objects.requireNonNull(logic);
+    }
+
+    private MatchBuilder maxHandSize(final int maxHandSizeValue) {
+        this.maxHandSize = maxHandSizeValue;
+        return this;
+    }
+
+    private MatchBuilder startingHandSize(final int startingHandSizeValue) {
+        this.startingHandSize = startingHandSizeValue;
+        return this;
+    }
+
+    private MatchBuilder playfieldSize(final int playfieldSizeValue) {
+        this.playfieldSize = playfieldSizeValue;
+        return this;
+    }
+
+    private MatchBuilder autoDraw(final boolean autoDrawValue) {
+        this.autoDraw = autoDrawValue;
+        return this;
+    }
+
+    private Match build() {
+        final Player player1 = new PlayerImpl(firstPlayerName, maxHandSize,
+                                                startingHandSize, firstPlayerDeck);
+        final Player player2 = new PlayerImpl(secondPlayerName, maxHandSize,
+                                                startingHandSize, secondPlayerDeck);
+
+        return new MatchImpl(player1, player2, playfieldSize, autoDraw, logic);
+    }
+}
 
 }
